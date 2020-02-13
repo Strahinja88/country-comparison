856488657249aa822733bb34ae70219c2aeb0a46
Strahinja <strahinjapantovic@ymail.com>
2020-02-13 23:45:42 +0100

    chart

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -17,7 +17,8 @@ class Dashboard extends Component {
       leastPopulated: "",
       difference: "",
       selected: false,
-      compareError: ""
+      compareError: "",
+      selectedCountries: []
     };
   }
 
@@ -75,41 +76,51 @@ class Dashboard extends Component {
   };
 
   compareCountries = () => {
-    const { checkboxes, countries } = this.state;
+    const { checkboxes } = this.state;
 
     if (checkboxes.length > 1) {
-      const promise = new Promise((resolve, reject) => {
-        for (let i = 0, n = checkboxes.length; i < n; i++) {
-          if (document.getElementById(checkboxes[i].name) !== null) {
-            document.getElementById(checkboxes[i].name).checked = false;
-          }
-          checkboxes[i].checked = false;
+      for (let i = 0, n = checkboxes.length; i < n; i++) {
+        if (document.getElementById(checkboxes[i].name) !== null) {
+          document.getElementById(checkboxes[i].name).checked = false;
         }
+        checkboxes[i].checked = false;
+      }
 
-        resolve(true);
-      });
+      const sortedCheckboxes = checkboxes.sort((a, b) =>
+        a.population > b.population ? -1 : 1
+      );
 
-      promise.then(() => {
-        const sortedCheckboxes = checkboxes.sort(
-          (a, b) => b.population - a.population
-        );
-        this.setState({
-          mostPopulated: sortedCheckboxes[0],
-          leastPopulated: sortedCheckboxes[sortedCheckboxes.length - 1],
-          difference:
-            sortedCheckboxes[0].population -
-            sortedCheckboxes[sortedCheckboxes.length - 1].population,
-          selected: true,
-          compareError: "",
-          searched: "",
-          checkboxes: []
-        });
+      this.setState({
+        mostPopulated: sortedCheckboxes[0],
+        leastPopulated: sortedCheckboxes[sortedCheckboxes.length - 1],
+        difference:
+          sortedCheckboxes[0].population -
+          sortedCheckboxes[sortedCheckboxes.length - 1].population,
+        selected: true,
+        compareError: "",
+        searched: "",
+        checkboxes: [],
+        selectedCountries: checkboxes
       });
     } else {
       this.setState({
         compareError: "You must select at least 2 countries"
       });
     }
+  };
+
+  populationPercentage = country => {
+    const { selectedCountries } = this.state;
+    let sum = 0;
+    let allPopulation = 0;
+    selectedCountries.map(c => {
+      allPopulation = sum += c.population;
+    });
+
+    console.log(allPopulation);
+    const populationPercentage = (country.population / allPopulation) * 100;
+
+    return Math.round(populationPercentage);
   };
 
   filtered = value => x =>
@@ -124,7 +135,8 @@ class Dashboard extends Component {
       leastPopulated,
       difference,
       selected,
-      compareError
+      compareError,
+      selectedCountries
     } = this.state;
 
     let isDisabled;
@@ -136,6 +148,10 @@ class Dashboard extends Component {
     }
 
     console.log(this.state);
+
+    const sortedSelectedCountries = selectedCountries.sort((a, b) =>
+      a.name > b.name ? 1 : -1
+    );
 
     return (
       <div>
@@ -164,7 +180,42 @@ class Dashboard extends Component {
                 />
               </div>
               <div className="row">
-                <div className="col-12 mt-5 country-chart">Country Chart</div>
+                <div className="col-12 mt-5 country-chart">
+                  <ul className="d-flex justify-content-around p-5 ml-5">
+                    {selected
+                      ? sortedSelectedCountries.map(country => (
+                          <li key={country.name} className="d-flex">
+                            <div className="square"></div>
+                            <p className="text-muted">{country.name}</p>
+                          </li>
+                        ))
+                      : null}
+                  </ul>
+                  <div className="row">
+                    <div className="col-md-2 scale">Chart</div>
+                    <div className="col-md-10 chart">
+                      <ul className="d-flex bd-highlight">
+                        {selected
+                          ? sortedSelectedCountries.map(country => (
+                              <li
+                                className="flex-fill bd-highlight"
+                                key={country.name}
+                              >
+                                <div
+                                  key={country.name}
+                                  className="chart-item text-center"
+                                >
+                                  <p className="p-2 text-white">
+                                    {this.populationPercentage(country)}%
+                                  </p>
+                                </div>
+                              </li>
+                            ))
+                          : null}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
