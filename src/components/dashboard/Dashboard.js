194f3e2b669e59c96c91f451c368663ac8bcea4c
Strahinja <strahinjapantovic@ymail.com>
2020-02-15 00:45:28 +0100

    scale finished

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -18,8 +18,7 @@ class Dashboard extends Component {
       difference: "",
       selected: false,
       compareError: "",
-      selectedCountries: [],
-      roundedNumber: null
+      selectedCountries: []
     };
   }
 
@@ -115,12 +114,76 @@ class Dashboard extends Component {
 
     let string = mostPopulated.population.toString();
 
+    let firstNumber = parseInt(string.substring(0, 1));
+    const secondNumber = parseInt(string.substring(1, 2));
+
+    if (secondNumber >= 5) {
+      firstNumber += 1;
+    }
+
     const roundedNumber = parseInt(
-      string.substring(0, 1) +
+      firstNumber.toString() +
         string.substring(1, string.length).replace(/[0-9]/g, "0")
     );
 
     return roundedNumber;
+  };
+
+  deviderCalc = () => {
+    const roundedNumber = this.roundNumberCalc();
+    const firstNumber = parseInt(roundedNumber.toString().substring(0, 1));
+    let number;
+
+    switch (firstNumber) {
+      case 1:
+      case 2:
+      case 5:
+        number = 10;
+        break;
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+        number = firstNumber;
+        break;
+      case 4:
+        number = 8;
+        break;
+      case 3:
+        number = 6;
+        break;
+      default:
+        break;
+    }
+
+    return number;
+  };
+
+  heightCalc = () => {
+    const devider = this.deviderCalc();
+    let height;
+
+    switch (devider) {
+      case 6:
+        height = 300;
+        break;
+      case 7:
+        height = 350;
+        break;
+      case 8:
+        height = 400;
+        break;
+      case 9:
+        height = 450;
+        break;
+      case 10:
+        height = 500;
+        break;
+      default:
+        break;
+    }
+
+    return height;
   };
 
   populationPercentage = country => {
@@ -141,7 +204,9 @@ class Dashboard extends Component {
 
     const roundedNumber = this.roundNumberCalc();
 
-    const height = `${(country.population / roundedNumber) * 500}px`;
+    const multipleHeight = this.heightCalc();
+
+    const height = `${(country.population / roundedNumber) * multipleHeight}px`;
 
     const obj = { populationPercentage, height, populationPercentage2 };
 
@@ -151,16 +216,17 @@ class Dashboard extends Component {
   scaleCalc = () => {
     const roundedNumber = this.roundNumberCalc();
 
-    const iterator = roundedNumber / 10;
+    const devider = this.deviderCalc();
+
+    console.log(devider);
+
+    const iterator = roundedNumber / devider;
 
     let arr = [];
 
     for (let i = roundedNumber; i >= 0; i -= iterator) {
       arr.push(i);
     }
-
-    // console.log(roundedNumber);
-    // console.log(arr);
     return arr;
   };
 
@@ -219,14 +285,16 @@ class Dashboard extends Component {
               <div className="row">
                 <div className="col-12 mt-5 pb-3 country-chart">
                   <ul className="d-flex justify-content-around p-5 ml-5">
-                    {selected
-                      ? selectedCountries.map(country => (
-                          <li key={country.name} className="d-flex">
-                            <div className="square"></div>
-                            <p className="text-muted">{country.name}</p>
-                          </li>
-                        ))
-                      : null}
+                    {selected ? (
+                      selectedCountries.map(country => (
+                        <li key={country.name} className="d-flex">
+                          <div className="square"></div>
+                          <p className="text-muted">{country.name}</p>
+                        </li>
+                      ))
+                    ) : (
+                      <strong>You have not selected countries yet.</strong>
+                    )}
                   </ul>
                   <div className="row">
                     <div className="col-2 d-flex flex-column justify-content-end">
@@ -244,33 +312,6 @@ class Dashboard extends Component {
                                 </li>
                               ))
                             : null}
-                          {/* <li>
-                            <span>40,000,000&#x2012;</span>
-                          </li>
-                          <li>
-                            <span>35,000,000&#x2012;</span>
-                          </li>
-                          <li>
-                            <span>30,000,000&#x2012;</span>
-                          </li>
-                          <li>
-                            <span>25,000,000&#x2012;</span>
-                          </li>
-                          <li>
-                            <span>20,000,000&#x2012;</span>
-                          </li>
-                          <li>
-                            <span>15,000,000&#x2012;</span>
-                          </li>
-                          <li>
-                            <span>10,000,000&#x2012;</span>
-                          </li>
-                          <li>
-                            <span>5,000,000&#x2012;</span>
-                          </li>
-                          <li>
-                            <span>0&#x2012;</span>{" "}
-                          </li> */}
                         </ul>
                       </div>
                     </div>
